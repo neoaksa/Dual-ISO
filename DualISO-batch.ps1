f4437989f8c56eda8fe13e8efc182f1e3c642f73
Jie Tao <34007729+neoaksa@users.noreply.github.com>
2020-07-24 21:37:04 -0400

    Update DualISO-batch.ps1
    add parallel for loop

--- DualISO-batch.ps1
+++ DualISO-batch.ps1
@@ -1,8 +1,7 @@
-Set-ExecutionPolicy unrestricted
 
 $dic_cr2hdr="C:\Users\Neo_a\Pictures\cr2hdr-win\"
-$dic_pic="C:\Users\Neo_a\Pictures\\20200724\"
+$dic_pic="C:\Users\Neo_a\Pictures\20200724\"
 
 Set-Location $dic_cr2hdr
 $file_name=Get-ChildItem $dic_pic
-$file_name | ForEach-Object {.\cr2hdr.exe $_ }
+$file_name | ForEach-Object -Parallel {.\cr2hdr.exe $_ }
