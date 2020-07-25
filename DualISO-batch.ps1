69b0c9f6d00fbf12225bfab412a130b4d02e0418
Jie Tao <34007729+neoaksa@users.noreply.github.com>
2020-07-24 21:32:12 -0400

    Create DualISO-batch.ps1

---
+++ DualISO-batch.ps1
@@ -0,0 +1,8 @@
+Set-ExecutionPolicy unrestricted
+
+$dic_cr2hdr="C:\Users\Neo_a\Pictures\cr2hdr-win\"
+$dic_pic="C:\Users\Neo_a\Pictures\\20200724\"
+
+Set-Location $dic_cr2hdr
+$file_name=Get-ChildItem $dic_pic
+$file_name | ForEach-Object {.\cr2hdr.exe $_ }
